054b87c175df873dc01285e6044754668f7f9c4c
Chefkoch Fern <meineMail@yahoo.com>
2025-01-22 15:56:59 +0100

    added music changing

--- Assets/Scripts/Music Singelton.cs
+++ Assets/Scripts/Music Singelton.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameMusicPlayer : MonoBehaviour
 {
@@ -9,6 +10,19 @@ public class GameMusicPlayer : MonoBehaviour
     {
         get { return instance; }
     }
+
+    private string currentScene = "";
+    private string lastScene = "MainMenu";
+
+    [SerializeField]
+    public AudioSource audioSource;
+
+
+    public AudioClip menuTheme;
+
+    public AudioClip gameTheme;
+
+
     void Awake()
     {
         if (instance != null && instance != this) {
@@ -23,6 +37,19 @@ public class GameMusicPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentScene = SceneManager.GetActiveScene().name;
+        if(currentScene == "MainMenu" || currentScene == "Credits" && lastScene != "MainMenu" && lastScene != "Credits"){
+            if(audioSource.clip != menuTheme){
+                audioSource.Pause();
+                audioSource.clip = menuTheme;
+                audioSource.Play();
+            }
+        }
+        else if(currentScene == "GameScene") {
+            audioSource.Pause();
+            audioSource.clip = gameTheme;
+            audioSource.Play();
+        }
+        lastScene = currentScene;
     }
 }
