d74a4276a19f976ea0beb0866b3a70f77cd3c1b0
Vivi <blauerapfel80@gmail.com>
2025-01-30 15:34:02 +0100

    enemy shoots functioning projectiles now

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.XR.CoreUtils;
@@ -6,8 +7,11 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
+    [SerializeField] GameObject projectile;
     private float minDistance = 1f;
     private float distance;
+    public float shootTimer = 2.0f;
+    public float projectileSpeed = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +22,29 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0.0f)
+        {
+            shoot();
+            shootTimer = 2.0f;
+        }
         distance = Vector3.Distance(transform.position, playerTransform.transform.position);
 
         if (distance > minDistance)
         {
-            transform.position = Vector3.MoveTowards(transform.position, playerTransform.transform.position, minDistance);
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, minDistance);
         }
+    }
+
+    void shoot()
+    {
+        GameObject projectileClone = Instantiate(projectile, transform.position, transform.rotation);
+        /*
+        Vector3 direction = playerTransform.position - transform.position;
+        print(direction);
+        direction = direction.normalized * projectileSpeed * Time.fixedDeltaTime;
+        projectileClone.velocity = direction; //transform.TransformDirection(Vector3.forward * 3);
+        */
+        //projectileClone.transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, 0.1f);
     }
 }
