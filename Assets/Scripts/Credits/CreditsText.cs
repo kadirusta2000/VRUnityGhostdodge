ab38a5ae883769d831be6fd6b5d1096870a9b9b2
Chefkoch Fern <meineMail@yahoo.com>
2025-02-14 22:50:44 +0100

    added how to play

--- Assets/Scripts/Credits/CreditsText.cs
+++ Assets/Scripts/Credits/CreditsText.cs
@@ -7,28 +7,24 @@ using TMPro;
 public class CreditsText : MonoBehaviour
 {
     public TMP_Text textMeshPro;
-    string[] creditsText;
-    string myFilePath;
 
-    void Start()
-    {
-        textMeshPro.text = null;
-        myFilePath = Application.dataPath + "/Misc/" + "attribution.txt";
-        ReadFromApplications();
+
+    public void howTo(){
+        textMeshPro.text =  "The Goal is quite simple: Survive! \nAt least least until your Ghosthunt device has charged up enough \nto fire the Ecto-Flashlight. \nTo do so you need to dodge the ghosts red bullets. \nBut beware!  you need to collect the green plasma shots with the \ndevice or they will hurt you aswell. \nUse the powerups around you to aid your mission by tapping them.";
     }
 
-    public void ReadFromApplications(){
-        creditsText = File.ReadAllLines(myFilePath);
-        foreach(string line in creditsText){
-            if(line.Equals("")){
-                textMeshPro.text += "\n";
-                print("empty");
-            }
-            else{
-                textMeshPro.text += line;
-            }
-            print(line);
-            }
+    public void Credits(){
+        textMeshPro.text = "A game by Vivian, Kadir and Mirkan"
+        + "\n\n3D Modelling: Burgerboy"
+        + "\n\nMusic: From Bensound.com/royalty-free-music"
+        + "\n *Main menu theme: Sci Fi by Benjamin Tissot"
+        + "\n *Game Theme: Moose by Benjamin Tissot"
+        + "\n\nSound Effects: Jes�s Lastra"
+        + "\n\nFonts:"
+        + "\n *Title: Funky Rundkopf Two - Nicks Fonts"
+        + "\n *UI: Zebulon - Pixel Sagas - Neale Davidson"
+        + "\n\nImages:"
+        + "\n *Cracks: https://www.pngfind.com/download/ibmhwT_png-file-size-cracked-line-png-transparent-png/";
         
     }
 }
